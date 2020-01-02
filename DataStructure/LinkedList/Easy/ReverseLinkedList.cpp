ca1c2bb33b7aee2bb4e5a163fe020cfc1a419517
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-03 02:36:45 +0600

    linked list

---
+++ DataStructure/LinkedList/Easy/ReverseLinkedList.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseList(struct ListNode* head){
+    
+    if(NULL == head || NULL == head -> next ) {
+        return head;
+    }
+    
+    struct ListNode* newHead = reverseList(head -> next);
+    
+    head -> next = NULL;
+    
+    struct ListNode* travel = newHead;
+    
+    while(NULL != travel -> next) {
+        travel = travel -> next;
+    }
+    
+    travel -> next = head;
+    
+    return newHead;
+}
