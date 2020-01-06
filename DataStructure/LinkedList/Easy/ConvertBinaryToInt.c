ee8bf2d40fb75364ddf70aa547d0b825193591fe
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-07 00:31:01 +0600

    linked list

---
+++ DataStructure/LinkedList/Easy/ConvertBinaryToInt.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+int getDecimalValue(struct ListNode* head){
+    int decimal = 0;
+    
+    while(NULL != head) {
+        
+        decimal = decimal * 2 + head -> val;
+        
+        head = head -> next;
+    }
+    
+    return decimal;
+}
