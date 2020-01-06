2e4dc1f97f3c4346e0b22dd76faf87d5fb8140ec
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-07 00:54:56 +0600

    linked list

---
+++ DataStructure/LinkedList/Easy/DeleteNodeLinkedList.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node -> val = node -> next -> val;
+        
+        while (NULL != node -> next -> next) {
+            node = node -> next;
+            
+            node -> val = node -> next -> val;
+        }
+        
+        node -> next = NULL;
+    }
+};
