f856124ecf75a7c2dbb874b9f97487753b779870
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-13 01:02:20 +0600

    string

--- String/Easy/RobotReturnToOrigin.c
+++ String/Easy/RobotReturnToOrigin.c
@@ -1,1 +1,22 @@
 
+bool judgeCircle(char * moves){
+    int posX = 0, posY = 0;
+    
+    for(int i = 0; moves[i] != '\0'; i++) {
+        if('L' == moves[i]) {
+            posX--;
+        } else if('R' == moves[i]) {
+            posX++;
+        } else if('D' == moves[i]) {
+            posY--;
+        } else if('U' == moves[i]) {
+            posY++;
+        }
+    }
+    
+    if(0 == posX && 0 == posY) {
+        return true;
+    }
+    
+    return false;
+}
