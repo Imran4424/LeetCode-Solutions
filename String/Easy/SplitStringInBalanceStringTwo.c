0d259d90ef2af88a70dae2dc2f28325ddcb8a7fd
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-13 00:50:28 +0600

    string

---
+++ String/Easy/SplitStringInBalanceStringTwo.c
@@ -0,0 +1,18 @@
+
+int balancedStringSplit(char * s){
+    int pairCount = 0, rCount = 0, lCount = 0;
+    for(int i = 0; s[i] != '\0'; i++) {
+        if('R' == s[i]) {
+            rCount++;
+        } else if('L' == s[i]) {
+            lCount++;
+        }
+        
+        if(0 != rCount && 0 != lCount && rCount == lCount) {
+            pairCount++;
+            rCount = lCount = 0;
+        }
+    }
+
+    return pairCount;
+}
