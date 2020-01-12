60a727d90893b74a2dc6e798e44b9b38a591780f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-13 00:50:00 +0600

    string

--- String/Easy/SplitStringInBalanceString.c
+++ String/Easy/SplitStringInBalanceString.c
@@ -1,7 +1,8 @@
 int balancedStringSplit(char * s){
     int pairCount = 0, rCount = 0, lCount = 0;
     for(int i = 0; s[i] != '\0'; i++) {
-        // we are checking at the begin of 
+        // we are checking at the begin of the loop
+        // for that reason we are unable to count the last occurence
         if(0 != rCount && 0 != lCount && rCount == lCount) {
             pairCount++;
             rCount = lCount = 0;
@@ -14,5 +15,6 @@ int balancedStringSplit(char * s){
         }        
     }
 
+    // returning with + 1 cause in loop we skip last count
     return pairCount + 1;
 }
