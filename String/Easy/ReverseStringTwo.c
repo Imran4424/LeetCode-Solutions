afef18433be88eae8fbbb796ed3ae56687dd9a77
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-13 01:06:55 +0600

    string

--- String/Easy/ReverseStringTwo.c
+++ String/Easy/ReverseStringTwo.c
@@ -4,6 +4,7 @@ void reverseString(char* s, int sSize){
     int availSize = sSize - 1;
     
     for(int i = 0; i < sSize / 2; i++) {
+    	/// using equation to swap two variables
         s[i] = s[i] + s[availSize - i];
         s[availSize - i] = s[i] - s[availSize - i];
         s[i] = s[i] - s[availSize - i];
