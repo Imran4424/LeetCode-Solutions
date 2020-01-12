b273f282910d9ae14fac06aeceaed2099f5db0f3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-13 00:52:10 +0600

    string

---
+++ String/Easy/ToLowerCase.c
@@ -0,0 +1,10 @@
+
+char * toLowerCase(char * str){
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + 32;
+        }
+    }
+    
+    return str;
+}
