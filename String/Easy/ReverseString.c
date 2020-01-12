ea7a29e7deb9f1e5502fc6710d5bce9b5915743a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-13 01:04:32 +0600

    string

--- String/Easy/ReverseString.c
+++ String/Easy/ReverseString.c
@@ -1,1 +1,11 @@
 
+void reverseString(char* s, int sSize){
+    
+    int availSize = sSize - 1;
+    
+    for(int i = 0; i < sSize / 2; i++) {
+        char temp = s[i];
+        s[i] = s[availSize - i];
+        s[availSize - i] = temp;
+    }
+}
