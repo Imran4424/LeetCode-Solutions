30d6bc861fca610008a3a4765147d9af7828be17
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-17 00:42:51 +0600

    string

---
+++ String/Easy/ReverseOnlyLetters.c
@@ -0,0 +1,18 @@
+char * reverseOnlyLetters(char * s) {
+    char tempStr[101];
+    int index = 0;
+    for(int i = 0; s[i] != '\0'; i++) {
+        if((s[i] >= 'A' && s[i] <= 'Z') || s[i] >= 'a' && s[i] <= 'z') {
+            tempStr[index++] = s[i];
+        }
+    }
+    
+    index--;
+    for(int i = 0; s[i] != '\0'; i++) {
+        if((s[i] >= 'A' && s[i] <= 'Z') || s[i] >= 'a' && s[i] <= 'z') {
+            s[i] = tempStr[index--];
+        }
+    }
+    
+    return s;
+}
