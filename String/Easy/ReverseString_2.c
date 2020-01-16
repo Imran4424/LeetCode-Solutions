2b9d3b900cce6b57a0df375be452e0fc7ab0397c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-17 00:44:22 +0600

    string

---
+++ String/Easy/ReverseString_2.c
@@ -0,0 +1,31 @@
+char * reverseStr(char * s, int k) {
+    int count = 1;
+    int lastStart = (count - 1) * k;
+    
+    
+    for(int i = 0; s[i] != '\0'; i++) {
+        if(i == lastStart) {
+            char tempStr[10001];
+            int index = 0;
+            // copying
+            for(int x = i; x < count * k && s[x] != '\0'; x++) {
+                tempStr[index++] = s[x];
+                 //printf("inside copy\n");
+            }
+
+           
+            
+            // reversing
+            int m, n;
+            for(m = index - 1, n = i; m >= 0 && n < count * k && s[n] != '\0'; m--, n++){
+                s[n] = tempStr[m];
+                //printf("inside reverse\n");
+            }
+
+            count = count + 2;
+            lastStart = (count - 1) * k;
+        }        
+    }
+    
+    return s;
+}
