fc71e5ab1bc26338caed6727f3278b1ff5b36569
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-25 22:25:01 +0600

    tree

---
+++ DataStructure/Tree/Easy/MergeTwoBinaryTrees.c
@@ -0,0 +1,27 @@
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* mergeTrees(struct TreeNode* treeOne, struct TreeNode* treeTwo) {
+    if(NULL == treeOne && NULL == treeTwo) {
+        return treeOne;
+    } else if(NULL == treeTwo) {
+        return treeOne;
+    } else if(NULL == treeOne) {
+        treeOne = treeTwo;
+    } else {
+        treeOne -> val = treeOne -> val + treeTwo -> val;
+        
+        treeOne -> left = mergeTrees(treeOne -> left, treeTwo -> left);
+        treeOne -> right = mergeTrees(treeOne -> right, treeTwo -> right);
+    }
+    
+    return treeOne;
+}
