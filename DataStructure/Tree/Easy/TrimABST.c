6b97444d16f24fca1fc6c11c36bcb7d7f70c6918
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-26 01:37:37 +0600

    tree

---
+++ DataStructure/Tree/Easy/TrimABST.c
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* trimBST(struct TreeNode* root, int L, int R){
+
+}
+
