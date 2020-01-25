34481f17b4b4e449c20dc24e68213643da91789e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-25 22:49:43 +0600

    tree

--- DataStructure/Tree/Easy/InvertBinaryTree.c
+++ DataStructure/Tree/Easy/InvertBinaryTree.c
@@ -1,1 +1,26 @@
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if(NULL == root) {
+        return root;
+    }
+    
+    struct TreeNode *temp = root -> left;
+    root -> left = root -> right;
+    root -> right = temp;
+    
+    root -> left = invertTree(root -> left);
+    root -> right = invertTree(root -> right);
+    
+    return root;
+}
+
