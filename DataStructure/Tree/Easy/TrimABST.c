fdde129692a906a28e34fd5f6c2e16a5b6b55163
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-01-26 01:53:30 +0600

    tree

--- DataStructure/Tree/Easy/TrimABST.c
+++ DataStructure/Tree/Easy/TrimABST.c
@@ -15,6 +15,34 @@ int MinValue(struct TreeNode* root) {
 	return MinValue(root -> left);
 }
 
+struct TreeNode* deleteNode(struct TreeNode* root, int target) {
+	if(NULL == root) {
+		return root;
+	}
+
+	if(target > root -> val) {
+		target -> right = deleteNode(root -> right, target);
+	} else if(target < root -> val) {
+		target -> left = deleteNode(root -> left, target);
+	} else {
+		if(NULL == root -> left && NULL == root -> right) {
+			root = NULL;
+		} else if(NULL == tree -> left) {
+			root = root -> right;
+		} else if(NULL == tree -> right) {
+			root = root -> left;
+		} else {
+			int replaceVal = MinValue(root -> right);
+
+			root -> val = replaceVal;
+
+			root -> right = deleteNode(root -> right, replaceVal);
+		}
+	}
+
+	return root;
+}
+
 struct TreeNode* trimBST(struct TreeNode* root, int L, int R){
 
 }
