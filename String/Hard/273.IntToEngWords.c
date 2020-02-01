e3689e4b515b5fc2e45281d348280a17a8ae039b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-01 16:36:24 +0600

    string

---
+++ String/Hard/273.IntToEngWords.c
@@ -0,0 +1,13 @@
+
+
+char * numberToWords(long long int num) {
+    int digits[12];
+    int index = 0;
+    
+    char *spelling = malloc(100 * sizeof(char));
+    
+    while(0 != num) {
+        digit[index++]
+    } 
+}
+
