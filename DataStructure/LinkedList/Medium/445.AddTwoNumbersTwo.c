760c696180559ed62b64805d3e305ba47707bf4b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-09 10:59:59 +0600

    linked list

--- DataStructure/LinkedList/Medium/445.AddTwoNumbersTwo.c
+++ DataStructure/LinkedList/Medium/445.AddTwoNumbersTwo.c
@@ -8,25 +8,110 @@
 
 struct ListNode* reverseList(struct ListNode* head){
     
-    if(NULL == head || NULL == head -> next) {
-        return head;
-    }
-    
+	if(NULL == head || NULL == head -> next) {
+		return head;
+	}
+
 	struct ListNode *newHead = NULL, *travel = head;
-    
-    while(NULL != travel) {
-        ListNode* InsertNode = new ListNode(travel -> val);
-        
-        InsertNode -> next = newHead;
-        newHead = InsertNode;
-        
-        travel = travel -> next;
-    }
-    
-    return newHead;
+
+	while(NULL != travel) {
+		ListNode* InsertNode = new ListNode(travel -> val);
+
+		InsertNode -> next = newHead;
+		newHead = InsertNode;
+
+		travel = travel -> next;
+	}
+
+	return newHead;
 }
 
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+struct ListNode* addTwoNumbers(struct ListNode* lisOne, struct ListNode* l2){
+	int carry = 0;
+	struct ListNode * newList = NULL;
+	struct ListNode *travel;
+    
+	while(NULL != listOne && NULL != listTwo) {
+		int digitOne = listOne -> val;
+		int digitTwo = listTwo -> val;
+
+		int result = digitOne + digitTwo + carry;
+
+		carry = result / 10;
+
+		struct ListNode *temp = malloc(sizeof(struct ListNode));
+		temp -> val = result % 10;
+		temp -> next = NULL;
+        
+		if(NULL == newList) {
+			newList = temp;
+			travel = newList;
+		} else {
+			while(NULL != travel -> next) {
+				travel = travel -> next;
+			}
+
+			travel -> next = temp;
+		}
+        
+        
+	        listOne = listOne -> next;
+	        listTwo = listTwo -> next;
+    	}
+    
+	while(NULL != listOne) {
+		int digitOne = listOne -> val;
+		int result = digitOne + carry;
+
+		carry = result / 10;
+
+		struct ListNode *temp = malloc(sizeof(struct ListNode));
+		temp -> val = result % 10;
+		temp -> next = NULL;
+
+		while(NULL != travel -> next) {
+			travel = travel -> next;
+		}
+	    
+		travel -> next = temp;
+
+		listOne = listOne -> next;
+	}
+    
+	while(NULL != listTwo) {
+	int digitTwo = listTwo -> val;
+	int result = digitTwo + carry;
+
+	carry = result / 10;
+
+	struct ListNode *temp = malloc(sizeof(struct ListNode));
+	temp -> val = result % 10;
+	temp -> next = NULL;
+
+	while(NULL != travel -> next) {
+	    travel = travel -> next;
+	}
+	    
+	travel -> next = temp;
+
+	listTwo = listTwo -> next;
+	}
+    
+    if(carry > 0) {
+        struct ListNode *temp = malloc(sizeof(struct ListNode));
+        temp -> val = carry;
+        temp -> next = NULL;
+       
+        //printf("Hi");
+        while(NULL != travel -> next) {
+            //printf("inside while");
+            travel = travel -> next;
+        }
+            
+        travel -> next = temp;
+    }
+    
+    return newList;
 
 }
 
