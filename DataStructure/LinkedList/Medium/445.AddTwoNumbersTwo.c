cbd4d3deb01e0e5897035329cdf66deec731c069
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-09 10:54:21 +0600

    linked list

--- DataStructure/LinkedList/Medium/445.AddTwoNumbersTwo.c
+++ DataStructure/LinkedList/Medium/445.AddTwoNumbersTwo.c
@@ -6,6 +6,25 @@
  * };
  */
 
+struct ListNode* reverseList(struct ListNode* head){
+    
+    if(NULL == head || NULL == head -> next) {
+        return head;
+    }
+    
+	ListNode *newHead = NULL, *travel = head;
+    
+    while(NULL != travel) {
+        ListNode* InsertNode = new ListNode(travel -> val);
+        
+        InsertNode -> next = newHead;
+        newHead = InsertNode;
+        
+        travel = travel -> next;
+    }
+    
+    return newHead;
+}
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
 
