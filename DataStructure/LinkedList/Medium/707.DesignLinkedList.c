224528f8f036f09ebca40b4ff4751508dedfaecb
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-15 09:14:46 +0600

    linked list

--- DataStructure/LinkedList/Medium/707.DesignLinkedList.c
+++ DataStructure/LinkedList/Medium/707.DesignLinkedList.c
@@ -2,56 +2,63 @@
 
 
 typedef struct MyLinkedList{
-	
+	int data;
+	struct MyLinkedList *next;
 } MyLinkedList;
 
 /** Initialize your data structure here. */
 
 MyLinkedList* myLinkedListCreate() {
-    
+	MyLinkedList *temp;
+
+	return temp;
 }
 
 /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
-int myLinkedListGet(MyLinkedList* obj, int index) {
+int myLinkedListGet(MyLinkedList* head, int index) {
   
 }
 
 /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
-void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
-  
+void myLinkedListAddAtHead(MyLinkedList* head, int val) {
+	MyLinkedList *temp = malloc(sizeof(MyLinkedList));
+	temp -> data = val;
+	temp -> next = head;
+
+	head = temp;
 }
 
 /** Append a node of value val to the last element of the linked list. */
-void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
+void myLinkedListAddAtTail(MyLinkedList* head, int val) {
   
 }
 
 /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
-void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
+void myLinkedListAddAtIndex(MyLinkedList* head, int index, int val) {
   
 }
 
 /** Delete the index-th node in the linked list, if the index is valid. */
-void myLinkedListDeleteAtIndex(MyLinkedList* obj, int index) {
+void myLinkedListDeleteAtIndex(MyLinkedList* head, int index) {
   
 }
 
-void myLinkedListFree(MyLinkedList* obj) {
+void myLinkedListFree(MyLinkedList* head) {
     
 }
 
 /**
  * Your MyLinkedList struct will be instantiated and called as such:
- * MyLinkedList* obj = myLinkedListCreate();
- * int param_1 = myLinkedListGet(obj, index);
+ * MyLinkedList* head = myLinkedListCreate();
+ * int param_1 = myLinkedListGet(head, index);
  
- * myLinkedListAddAtHead(obj, val);
+ * myLinkedListAddAtHead(head, val);
  
- * myLinkedListAddAtTail(obj, val);
+ * myLinkedListAddAtTail(head, val);
  
- * myLinkedListAddAtIndex(obj, index, val);
+ * myLinkedListAddAtIndex(head, index, val);
  
- * myLinkedListDeleteAtIndex(obj, index);
+ * myLinkedListDeleteAtIndex(head, index);
  
- * myLinkedListFree(obj);
+ * myLinkedListFree(head);
 */
