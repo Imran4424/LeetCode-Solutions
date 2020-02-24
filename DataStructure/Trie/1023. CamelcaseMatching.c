042c552d274061c9e69d7597b4d247827e845518
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-25 02:08:15 +0600

    trie

--- DataStructure/Trie/1023. CamelcaseMatching.c
+++ DataStructure/Trie/1023. CamelcaseMatching.c
@@ -6,13 +6,15 @@
 bool* camelMatch(char ** queries, int queriesSize, char * pattern, int* returnSize){
 	bool *arr = malloc(queriesSize * sizeof(bool));
 
-	int pIndex = 0, bIndex = 0;
+	int bIndex = 0;
 	for (int i = 0; i < queriesSize; i++) {
 		
 		bool status = true;
+		int pIndex = 0;
 		for (int x = 0; queries[i][x]; x++) {
-			if(queries[x][i] >= 'A' && queries[i][x] <= 'Z') {
-				if(queries[x][i] != pattern[pIndex]) {
+			if(queries[i][x] >= 'A' && queries[i][x] <= 'Z') {
+				
+				if('\0' == pattern[pIndex] || queries[i][x] != pattern[pIndex]) {
 					status = false;
 					break;
 				} else {
