48218b82eea0d5fd08ef66f1baef112e74a443e9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-24 23:47:32 +0600

    trie

--- DataStructure/Trie/677.MapSumPairs.c
+++ DataStructure/Trie/677.MapSumPairs.c
@@ -1,15 +1,17 @@
 
 
+const int tSize = 26;
 
 typedef struct MapSum{
 	bool endMark;
 	int nodeVal;
+	struct MapSum* next[26];
 } MapSum;
 
 /** Initialize your data structure here. */
 
 MapSum* mapSumCreate() {
-    
+	MapSum* root = malloc(sizeof(MapSum));
 }
 
 void mapSumInsert(MapSum* obj, char * key, int val) {
