3df9381cfe531f77416dacca71de91380c33044b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-24 22:13:20 +0600

    trie

--- DataStructure/Trie/208.ImplementTrie(PrefixTree).c
+++ DataStructure/Trie/208.ImplementTrie(PrefixTree).c
@@ -1,33 +1,78 @@
 
-
+const int tSize = 26;
 
 typedef struct Trie {
-    
+	bool endMark;
+	struct Trie* next[tSize];
 } Trie;
 
 /** Initialize your data structure here. */
 
 Trie* trieCreate() {
-    
+	Trie* root = malloc(sizeof(Trie));
+
+	root -> endMark = false;
+
+	for (int i = 0; i < tSize; ++i) {
+		root -> next[i] = NULL;
+	}
+
+	return root;
 }
 
 /** Inserts a word into the trie. */
-void trieInsert(Trie* obj, char * word) {
-  
+void trieInsert(Trie* travel, char *word) {
+	for (int i = 0; word[i]; i++) {
+		int letter = word[i] - 'a';
+
+		if(NULL == travel -> next[letter]) {
+			travel -> next = trieCreate();
+		}
+
+		travel = travel -> next[letter];
+	}
+
+	travel -> endMark = true;
 }
 
 /** Returns if the word is in the trie. */
-bool trieSearch(Trie* obj, char * word) {
-  
+bool trieSearch(Trie* travel, char * word) {
+	for (int i = 0; i < word[i]; i++) {
+		int letter = word[i] - 'a';
+
+		if(NULL == travel -> next[letter]) {
+			return false;
+		}
+
+		travel = travel -> next[i];
+	}
+
+	return travel -> endMark;
 }
 
 /** Returns if there is any word in the trie that starts with the given prefix. */
-bool trieStartsWith(Trie* obj, char * prefix) {
-  
+bool trieStartsWith(Trie* travel, char * prefix) {
+	for (int i = 0; prefix[i]; i++) {
+		int letter = word[i] - 'a';
+
+		if(NULL == travel -> next[letter]) {
+			return false;
+		}
+
+		travel = travel - next[letter];
+	}
+
+	for(int i = 0 ; i < tSize; i++) {
+		if(NULL != travel -> next[i]) {
+			return true;
+		}
+	}
+
+	return travel -> endMarkl;
 }
 
 void trieFree(Trie* obj) {
-    
+	free(obj);
 }
 
 /**
