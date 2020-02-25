d926ebf4f56a0f0718436653ae5f73fe8d59aee5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-26 00:59:53 +0600

    heap

--- DataStructure/Heap/378. Kth Smallest Element in a Sorted Matrix.c
+++ DataStructure/Heap/378. Kth Smallest Element in a Sorted Matrix.c
@@ -1,8 +1,50 @@
 
+int Absolute(int x) {
+	if(x < 0) {
+		return -x;
+	}
 
-int kthSmallest(int** matrix, int matrixSize, int* matrixColSize, int k){
-	int row = matrixSize, col = *matrixColSize;
+	return x;
+}
 
+int kthSmallest(int** matrix, int matrixSize, int* matrixColSize, int target){
+	int rowSize = matrixSize;
+	int colSize = *matrixColSize;
+
+	int lowRow = 1, highRow = rowSize;
+	int lowCol = 1, highCol = colSize;
+
+	int midRow, midCol, calRow;
+
+	while(1) {
+		midRow = lowRow + (highRow - lowRow) / 2;
+
+		calRow = (midRow * colSize);
+		int diffRow = Absolute(calRow - target);
+
+		if(diffRow < colSize) {
+			break;
+		} else if(target > calRow) {
+			lowRow = midRow + 1;
+		} else {
+			highRow = midRow - 1;
+		}
+	}
 	
+	while(1) {
+		midCol = lowCol + (highCol - lowCol) / 2;
+
+		int calTotal = calRow + midCol;
+
+		if(target == calTotal) {
+			break;
+		} else if (target > calTotal) {
+			lowCol = midCol + 1;
+		} else {
+			highCol = midCol - 1;
+		}
+	}
+
+	return matrix[midRow - 1][midCol - 1];
 }
 
