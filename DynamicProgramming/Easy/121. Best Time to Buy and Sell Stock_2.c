7f8606cf78dd24b39590e3ab2429b54d12349cef
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-03 14:00:00 +0600

    dynamic programming

--- DynamicProgramming/Easy/121. Best Time to Buy and Sell Stock_2.c
+++ DynamicProgramming/Easy/121. Best Time to Buy and Sell Stock_2.c
@@ -1,3 +1,18 @@
 /*
 	One Pass
 */
+
+int maxProfit(int* prices, int pricesSize) {
+	int firstMinBeforeMax = 0;
+	int i;
+	for(i = 0; i < pricesSize - 1; i++) {
+		if(prices[i] < prices[i + 1]) {
+			firstMinBeforeMax = prices[i];
+			break;
+		}
+	}
+
+	for(; i < pricesSize; i++) {
+		
+	}
+}
