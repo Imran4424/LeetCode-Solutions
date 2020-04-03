db559f3fa8707d2489abfeaaebc936bed61a3d17
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-03 20:55:28 +0600

    dynamic programming

---
+++ DynamicProgramming/Easy/70. Climbing Stairs_2.c
@@ -0,0 +1,14 @@
+
+int pathCount[100] = {0};
+
+int climbStairs(int number) {
+	if(0 != pathCount[number]) {
+		return pathCount[number];
+	}
+    
+	if(0 == number || 1 == number) {
+		return pathCount[number] = 1;
+	}
+
+	return pathCount[number] = climbStairs(number - 1) + climbStairs(number - 2);
+}
