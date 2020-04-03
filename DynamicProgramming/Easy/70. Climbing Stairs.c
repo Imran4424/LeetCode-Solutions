4112b6205b6f276aa5252d375df1415601d284b3
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-03 20:53:56 +0600

    dynamic programming

--- DynamicProgramming/Easy/70. Climbing Stairs.c
+++ DynamicProgramming/Easy/70. Climbing Stairs.c
@@ -1,7 +1,7 @@
 
 int pathCount[1000] = {-1};
 
-int climbStairs(int number) {
+int climbStairsRecursive(int number) {
 	if(-1 != pathCount[number]) {
 		return pathCount[number];
 	}
@@ -10,5 +10,13 @@ int climbStairs(int number) {
 		return pathCount[number] = 1;
 	}
 
-	return pathCount[number] = climbStairs(number - 1) + climbStairs(number - 2);
+	return pathCount[number] = climbStairsRecursive(number - 1) + climbStairsRecursive(number - 2);
+}
+
+int climbStairs(int number) {
+	for(int i = 0 ; i < 1000; i++) {
+		pathCount[i] = -1;
+	}
+
+	return climbStairsRecursive(number);
 }
