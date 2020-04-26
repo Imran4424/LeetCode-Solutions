6077fa6429dde5cb9c7e26944fb42f131b0db196
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-26 19:07:40 +0600

    tree to Str

---
+++ DataStructure/Tree/Easy/606. Construct String from Binary Tree.c
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+char *nodeStr;
+
+
+char * tree2str(struct TreeNode* t) {
+	nodeStr = malloc(sizeof(char));
+}
+
