8ad552e88fec8b5f667c93a76245a6dab64629a1
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-26 12:21:25 +0600

    trim a BST

--- DataStructure/Tree/Easy/669.TrimABST_2.c
+++ DataStructure/Tree/Easy/669.TrimABST_2.c
@@ -8,7 +8,20 @@
  */
 
 
-struct TreeNode* trimBST(struct TreeNode* root, int L, int R){
+struct TreeNode* trimBST(struct TreeNode* root, int high, int low) {
+	if(NULL == root) {
+		return root;
+	}
 
+	if(low < root -> val) {
+		root = trimBST(root -> left, high, low);
+	}
+
+	if(high > root -> val) {
+		root = trimBST(root -> right, high, low);
+	}
+
+	root -> left = trimBST(root -> left, high, low);
+	root -> right =trimBST(root -> right, high, low);
 }
 
