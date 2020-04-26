42bfa57090180f63a909c8fdabef57305385bc5f
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-26 19:27:35 +0600

    tree to Str

--- DataStructure/Tree/Easy/606. Construct String from Binary Tree.c
+++ DataStructure/Tree/Easy/606. Construct String from Binary Tree.c
@@ -10,12 +10,24 @@
 char *nodeStr;
 int strIndex;
 
+int Absolute(int x) {
+	if(x < 0) {
+		return -x;
+	}
+
+	return x;
+}
+
 void treeToString(struct TreeNode* root) {
 	if(NULL == root) {
 		return;
 	}
 
-	nodeStr[strIndex++] = (char)root -> val + 48;
+	if(root -> val < 0) {
+		nodeStr[strIndex++] = '-';
+	}
+
+	nodeStr[strIndex++] = (char) Absolute(root -> val) + 48;
 
 	if(NULL == root -> left && NULL == root -> right) {
 		return;
