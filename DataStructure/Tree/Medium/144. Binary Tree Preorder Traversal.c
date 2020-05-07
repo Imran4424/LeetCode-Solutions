95dd392554b28e5b23c2489293cc7bac878c7293
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-07 11:43:27 +0600

    Tree

---
+++ DataStructure/Tree/Medium/144. Binary Tree Preorder Traversal.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int tIndex;
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* preorderTraversal(struct TreeNode* root, int* returnSize){
+
+}
