aceadeb0890e515e21c35803728fb33540d84213
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-07 12:07:40 +0600

    Tree

--- DataStructure/Tree/Medium/94. Binary Tree Inorder Traversal.c
+++ DataStructure/Tree/Medium/94. Binary Tree Inorder Traversal.c
@@ -7,10 +7,32 @@
  * };
  */
 
+int tIndex;
+
+void inOrder(struct TreeNode* root, int* values) {
+	if(NULL == root) {
+		return;
+	}	
+
+	inOrder(root -> left, values);
+
+	values[tIndex++] = root -> val;
+
+	inOrder(root -> right, values);
+}
+
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* inorderTraversal(struct TreeNode* root, int* returnSize){
+	tIndex = 0;
 
+	int *values = malloc(1000 * sizeof(int));
+
+	inOrder(root, values);
+
+	*returnSize = tIndex;
+
+	return values;
 }
