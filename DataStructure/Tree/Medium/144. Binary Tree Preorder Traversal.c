45afb86b20d94e2bb8c9702cdfd961f3cbb964cd
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-07 11:52:50 +0600

    Tree

--- DataStructure/Tree/Medium/144. Binary Tree Preorder Traversal.c
+++ DataStructure/Tree/Medium/144. Binary Tree Preorder Traversal.c
@@ -9,10 +9,29 @@
 
 int tIndex;
 
+void preOrder(struct TreeNode* root, int* values) {
+	if(NULL == root) {
+		return;
+	}
+
+	values[tIndex++] = root -> val;
+	
+	preOrder(root -> left, values);
+	preOrder(root -> right, values);
+}
+
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* preorderTraversal(struct TreeNode* root, int* returnSize){
 	tIndex = 0;
+
+	int *values = maloc(1000 * sizeof(int));
+
+	preOrder(root, values);
+
+	*returnSize = tIndex;
+
+	return values;
 }
