2308df592673f10d3ac1f0f41df7485108d3465d
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-07 13:18:59 +0600

    Tree

--- DataStructure/Tree/Easy/783. Minimum Distance Between BST Nodes.c
+++ DataStructure/Tree/Easy/783. Minimum Distance Between BST Nodes.c
@@ -7,6 +7,19 @@
  * };
  */
 
+int tIndex;
+
+void getValues(struct TreeNode* root, int* values) {
+	if(NULL == root) {
+		return;
+	}
+
+	values[tIndex++] = root -> val;
+
+	getValues(root -> left, values);
+	getValues(root -> right, values);
+}
+
 
 int minDiffInBST(struct TreeNode* root){
 
