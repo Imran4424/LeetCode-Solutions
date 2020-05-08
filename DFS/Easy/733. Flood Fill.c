8608d158646a409fc55cda044c54365218d14fbc
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-08 15:12:33 +0600

    dfs

--- DFS/Easy/733. Flood Fill.c
+++ DFS/Easy/733. Flood Fill.c
@@ -7,22 +7,22 @@ void floodFillHelper(int** image, int x, int y, int newColor) {
 	image[x][y] = newColor;
 
 	// checking upper
-	if(x - 1 >= 0 && 1 == image[x - 1][y]) {
+	if(x - 1 >= 0 && activeVal == image[x - 1][y]) {
 		floodFillHelper(image, x - 1, y, newColor);
 	}
 
 	// checking lower
-	if(x + 1 < sizeX && 1 == image[x + 1][y]) {
+	if(x + 1 < sizeX && activeVal == image[x + 1][y]) {
 		floodFillHelper(image, x + 1, y, newColor);
 	}
 
 	// checking left
-	if(y - 1 >= 0 && 1 == image[x][y - 1]) {
+	if(y - 1 >= 0 && activeVal == image[x][y - 1]) {
 		floodFillHelper(image, x, y - 1, newColor);
 	}
 
 	// checking right
-	if(y + 1 < sizeY && 1 == image[x][y + 1]) {
+	if(y + 1 < sizeY && activeVal == image[x][y + 1]) {
 		floodFillHelper(image, x, y + 1, newColor);
 	}
 }
