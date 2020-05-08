faafd8e13c38ded191e885a6cd74370e897165b2
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-08 14:39:28 +0600

    dfs

---
+++ DFS/Easy/733. Flood Fill.c
@@ -0,0 +1,15 @@
+
+
+int sizeX, sizeY;
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** floodFill(int** image, int imageSize, int* imageColSize, int sr, int sc, int newColor, int* returnSize, int** returnColumnSizes){
+
+	*returnSize = imageSize;
+	*returnColumnSizes = returnSize;
+	return image;
+}
