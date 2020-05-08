3ccacb01e20c65010ac5e039ef2750845157baf3
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-08 14:50:45 +0600

    dfs

--- DFS/Easy/733. Flood Fill.c
+++ DFS/Easy/733. Flood Fill.c
@@ -14,6 +14,16 @@ void floodFillHelper(int** image, int x, int y, int newColor) {
 	if(x + 1 < sizeX && 1 == image[x + 1][y]) {
 		floodFillHelper(image, x + 1, y, newColor);
 	}
+
+	// checking left
+	if(y - 1 >= 0 && 1 == image[x][y - 1]) {
+		floodFillHelper(image, x, y - 1, newColor);
+	}
+
+	// checking right
+	if(y + 1 < sizeY && 1 == image[x][y + 1]) {
+		floodFillHelper(image, x, y + 1, newColor);
+	}
 }
 
 /**
@@ -22,6 +32,10 @@ void floodFillHelper(int** image, int x, int y, int newColor) {
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 int** floodFill(int** image, int imageSize, int* imageColSize, int sr, int sc, int newColor, int* returnSize, int** returnColumnSizes){
+	sizeX = imageSize;
+	sizeY = imageColSize;
+
+	
 
 	*returnSize = imageSize;
 	*returnColumnSizes = returnSize;
