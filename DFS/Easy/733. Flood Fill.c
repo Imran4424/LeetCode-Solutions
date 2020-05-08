940305c226b87e6008322f8a8d8945a1fb55acac
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-08 15:27:47 +0600

    dfs

--- DFS/Easy/733. Flood Fill.c
+++ DFS/Easy/733. Flood Fill.c
@@ -2,27 +2,29 @@
 
 int sizeX, sizeY;
 int activeVal;
+int **visited;
 
 void floodFillHelper(int** image, int x, int y, int newColor) {
 	image[x][y] = newColor;
+	visited[x][y] = 1;
 
 	// checking upper
-	if(x - 1 >= 0 && activeVal == image[x - 1][y]) {
+	if(x - 1 >= 0 && activeVal == image[x - 1][y] && !visited[x - 1][y]) {
 		floodFillHelper(image, x - 1, y, newColor);
 	}
 
 	// checking lower
-	if(x + 1 < sizeX && activeVal == image[x + 1][y]) {
+	if(x + 1 < sizeX && activeVal == image[x + 1][y] && !visited[x + 1][y]) {
 		floodFillHelper(image, x + 1, y, newColor);
 	}
 
 	// checking left
-	if(y - 1 >= 0 && activeVal == image[x][y - 1]) {
+	if(y - 1 >= 0 && activeVal == image[x][y - 1] && !visited[x][y - 1]) {
 		floodFillHelper(image, x, y - 1, newColor);
 	}
 
 	// checking right
-	if(y + 1 < sizeY && activeVal == image[x][y + 1]) {
+	if(y + 1 < sizeY && activeVal == image[x][y + 1] && !visited[x][y + 1]) {
 		floodFillHelper(image, x, y + 1, newColor);
 	}
 }
@@ -36,6 +38,15 @@ int** floodFill(int** image, int imageSize, int* imageColSize, int sr, int sc, i
 	sizeX = imageSize;
 	sizeY = *imageColSize;
 	activeVal = image[sr][sc];
+
+	visited = malloc(sizeX * sizeof(int*));
+	for(int i = 0; i < sizeX; i++) {
+		visited[i] = malloc(sizeY * sizeof(int));
+
+		for(int j = 0; j < sizeY; j++) {
+			visited[i][j] = 0;
+		}
+	}
 
 	floodFillHelper(image, sr, sc, newColor);
 
