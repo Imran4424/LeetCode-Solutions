aff47de75059f17f8801b135413a8d5e973dad0d
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-06-11 19:52:02 +0600

    array

--- DataStructure/Array/Easy/1470. Shuffle the Array.c
+++ DataStructure/Array/Easy/1470. Shuffle the Array.c
@@ -6,5 +6,12 @@
 int* shuffle(int* nums, int numsSize, int n, int* returnSize){
 	int *newNums = malloc(numsSize * sizeof(int));
 
-	
+	int i, j, index = 0;
+	for(i = 0, j = n; index < numsSize - 1; i++, j++) {
+		newNums[index++] = nums[i];
+		newNums[index++] = nums[j];
+	}
+
+	*returnSize = numsSize;
+	return newNums;
 }
