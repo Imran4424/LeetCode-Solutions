3618a692033aa70f904c3cc8e7693a09362be780
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-06-11 20:02:03 +0600

    array

--- DataStructure/Array/Easy/1431. Kids With the Greatest Number of Candies.c
+++ DataStructure/Array/Easy/1431. Kids With the Greatest Number of Candies.c
@@ -4,5 +4,12 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize){
+	bool *canGreatest = malloc(candiesSize * sizeof(bool));
 
+	int maxCandie = candies[0];
+	for(int i = 1; i < candiesSize; i++) {
+		if(candies[i] > maxCandie) {
+			maxCandie = candies[i];
+		}
+	}
 }
