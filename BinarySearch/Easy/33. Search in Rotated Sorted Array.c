8ac98b1d5c184c97ad0d8c65b4615c3ac8255398
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-06-24 13:59:05 +0600

    binary search

---
+++ BinarySearch/Easy/33. Search in Rotated Sorted Array.c
@@ -0,0 +1,12 @@
+
+
+int search(int* nums, int numsSize, int target){
+    for(int i = 0; i < numsSize; i++) {
+        if(nums[i] == target){
+            return i;
+        }
+    }
+    
+    return -1;
+}
+
