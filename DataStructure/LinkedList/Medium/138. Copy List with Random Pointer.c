535d13b2916938f82b16903e2c781803dc8714e0
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-21 12:34:06 +0600

    copy with random pointer

---
+++ DataStructure/LinkedList/Medium/138. Copy List with Random Pointer.c
@@ -0,0 +1,12 @@
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     struct TreeNode *next;
+ *     struct TreeNode *random;
+ * };
+ */
+
+struct Node* copyRandomList(struct Node* head) {
+    return head;
+}
