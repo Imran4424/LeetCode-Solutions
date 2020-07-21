f77e6022c810df817a64a4acb5382438975493ab
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-21 13:58:39 +0600

    copy with random pointer

--- DataStructure/LinkedList/Medium/138. Copy List with Random Pointer.c
+++ DataStructure/LinkedList/Medium/138. Copy List with Random Pointer.c
@@ -7,6 +7,40 @@
  * };
  */
 
+ /*
+	this link has the solution concept
+	https://www.youtube.com/watch?v=EHpS2TBfWQg
+ */
+
+typedef struct Node Node;
+
 struct Node* copyRandomList(struct Node* head) {
-    return head;
+    if(NULL == head) {
+    	return head;
+    }
+
+    Node *newListHead = malloc(sizeof(Node));
+    newListHead -> val = head -> val;
+    newListHead -> next = head -> next;
+    newListHead -> random = head;
+
+    Node* prev = head;
+    Node* travel = head -> next;
+    Node* newTravel = newListHead;
+    prev -> next = head;
+
+    while(NULL != travel) {
+    	Node* temp = malloc(sizeof(Node));
+    	temp -> val = travel -> val;
+    	temp -> next = travel -> next;
+    	temp -> random = travel;
+
+    	newTravel -> next = temp;
+
+    	travel = travel -> next;
+    	newTravel = newTravel -> next;
+    }
+
+
+    return newListHead;
 }
