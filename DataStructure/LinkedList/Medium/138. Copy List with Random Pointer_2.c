c41930c946aa7bceaeec923b4bdbe58ea3fc0628
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-21 16:50:36 +0600

    copy with random pointer

---
+++ DataStructure/LinkedList/Medium/138. Copy List with Random Pointer_2.c
@@ -0,0 +1,76 @@
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     struct TreeNode *next;
+ *     struct TreeNode *random;
+ * };
+ */
+
+ /*
+	this link has the solution concept
+	https://www.youtube.com/watch?v=EHpS2TBfWQg
+ */
+
+typedef struct Node Node;
+
+struct Node* copyRandomList(struct Node* head) {
+    if(NULL == head) {
+    	return head;
+    }
+
+    // copying the linked list
+    // setting random pointer of copy list to main list respective nodes
+
+    Node *newListHead = malloc(sizeof(Node));
+    newListHead -> val = head -> val;
+    newListHead -> next = head -> next;
+    newListHead -> random = head;
+    
+    Node* travel = head -> next;
+    Node* copyTravel = newListHead;
+ 
+    while(NULL != travel) {
+    	Node* temp = malloc(sizeof(Node));
+    	temp -> val = travel -> val;
+    	temp -> next = travel -> next;
+    	temp -> random = travel;
+
+    	copyTravel -> next = temp;
+
+    	travel = travel -> next;
+    	copyTravel = copyTravel -> next;
+    }
+
+    // set main list next pointer to respective copy list node
+
+    Node* prev = head;
+    travel = head;
+    copyTravel = newListHead;
+
+    while(NULL != travel) {
+    	travel = travel -> next;
+
+    	prev -> next = copyTravel;
+    	prev = travel;
+
+    	copyTravel = copyTravel -> next;
+    }
+
+    // set copy list random pointer to main list random -> next pointer
+
+    copyTravel = newListHead;
+
+    while(NULL != copyTravel) {
+    	// using this rule
+    	if(NULL == copyTravel -> random -> random) {
+    		copyTravel -> random = NULL;
+    	} else {
+	    	copyTravel -> random = copyTravel -> random -> random -> next;
+    	}
+
+    	copyTravel = copyTravel -> next;
+    }
+
+    return newListHead;
+}
