d6dd0a16ca2c8e99b33c7f5ecd444e89578b25cd
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-23 22:43:58 +0600

    intersection of two lists

--- DataStructure/LinkedList/Easy/160. Intersection of Two Linked Lists_3.c
+++ DataStructure/LinkedList/Easy/160. Intersection of Two Linked Lists_3.c
@@ -6,6 +6,16 @@
  *     struct ListNode *next;
  * };
  */
+
+typedef struct ListNode ListNode;
+
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
-    
+	if(NULL == headA || NULL == headB) {
+		return NULL;
+	}
+
+	ListNode* travelA = headA;
+	ListNode* travelB = headB;  
+
+	
 }
