4277a0476991a34297491c2e50df6cc81ad9954d
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-29 13:08:57 +0600

    sqrt(X)

--- BinarySearch/Easy/69. Sqrt(x)_2.c
+++ BinarySearch/Easy/69. Sqrt(x)_2.c
@@ -5,5 +5,17 @@ int mySqrt(int x) {
 	int low = 1;
 	int high = x / 2;
 
-	
+	while(low < high) {
+		int mid = low + (high - low) / 2;
+
+		if (mid * mid == x) {
+			return mid;
+		} else if(mid * mid > x) {
+			high = mid - 1;
+		} else {
+			low = mid + 1;
+		}
+	}
+
+	return high;
 }
