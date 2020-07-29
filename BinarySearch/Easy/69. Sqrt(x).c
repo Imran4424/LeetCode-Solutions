a2484dd865d99cddda7e74333c826a71786aaf35
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-29 11:12:36 +0600

    sqrt(X)

---
+++ BinarySearch/Easy/69. Sqrt(x).c
@@ -0,0 +1,15 @@
+
+
+
+int mySqrt(int num){
+    
+    long long int i;
+    
+    for(i = 1; i*i <= num; i++) {
+        if(i*i == num) {
+            return i;
+        }
+    }
+    
+    return i - 1;
+}
