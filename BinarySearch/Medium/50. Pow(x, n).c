c66c15b494928497b3af1501628ac52e114ff25b
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-29 19:27:15 +0600

    Pow(X, n)

--- BinarySearch/Medium/50. Pow(x, n).c
+++ BinarySearch/Medium/50. Pow(x, n).c
@@ -3,7 +3,15 @@
 double myPow(double x, int n) {
 	if(n < 0) {
 		n = -n;
-		x = 1/x;
+		x = 1 / x;
+	}
+
+	double poweredNum = x;
+
+	while(n > 1) {
+		poweredNum = poweredNum * poweredNum;
+
+		n = n / 2; 
 	}
 
 	return poweredNum;
