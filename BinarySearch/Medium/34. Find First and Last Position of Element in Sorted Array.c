56b17a073f1ff5abf2578415d7132d16cbd8ce13
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-30 00:07:26 +0600

    Pow(X, n)

--- BinarySearch/Medium/34. Find First and Last Position of Element in Sorted Array.c
+++ BinarySearch/Medium/34. Find First and Last Position of Element in Sorted Array.c
@@ -3,6 +3,6 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* searchRange(int* nums, int numsSize, int target, int* returnSize){
+int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
 
 }
