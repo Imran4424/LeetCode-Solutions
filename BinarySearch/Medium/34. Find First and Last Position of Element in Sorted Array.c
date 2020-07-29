2a13d107d885b218db3dc4183367aea4b4aa4049
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-30 00:23:28 +0600

    Find First and Last Position of Element in Sorted Array

--- BinarySearch/Medium/34. Find First and Last Position of Element in Sorted Array.c
+++ BinarySearch/Medium/34. Find First and Last Position of Element in Sorted Array.c
@@ -6,6 +6,8 @@
 int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
 	int *indexArr = malloc(2 * sizeof(int));
 
+	indexArr[0] = indexArr[1] = -1;
+
 	int index = 0;
 
 	for (int i = 0; i < numsSize; i++) {
