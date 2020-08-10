a683e09aaa44c81d3f49c72f3bda90292ece151e
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-08-10 14:52:48 +0600

    first and last index

--- BinarySearch/Medium/34. Find First and Last Position of Element in Sorted Array_2.c
+++ BinarySearch/Medium/34. Find First and Last Position of Element in Sorted Array_2.c
@@ -7,7 +7,17 @@ int findElement(int* nums, int numsSize, int target) {
 
 	while(low < high) {
 		int mid = low + (high - low) / 2;
+
+		if(target == nums[mid]) {
+			return mid;
+		} else if(target > nums[mid]) {
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
 	}
+
+	return -1;
 }
 
 
