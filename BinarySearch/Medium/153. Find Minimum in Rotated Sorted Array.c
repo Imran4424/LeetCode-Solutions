238ebc5b5cee5de39e11bc92901827beaf5d72fb
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-08-19 09:57:58 +0600

    find minimum in rotated sorted array

--- BinarySearch/Medium/153. Find Minimum in Rotated Sorted Array.c
+++ BinarySearch/Medium/153. Find Minimum in Rotated Sorted Array.c
@@ -1,8 +1,14 @@
 
-int findMin(int* nums, int numsSize) {
-	int min = nums[numsSize - 1];
+int findminValue(int* nums, int numsSize) {
+	int minValue = nums[numsSize - 1];
 
 	for(int i = numsSize - 1; i >= 0; i--) {
-		
+		if(nums[i] < minValue) {
+			minValue = nums[i];
+		} else if (nums[i] > minValue) {
+			break;
+		}
 	}
+
+	return minValue;
 }
