eb3e999ca60ea22c7fcf97247bb8ba61a52a4b78
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-08-20 08:19:58 +0600

    find minimum in rotated sorted array

--- BinarySearch/Medium/153. Find Minimum in Rotated Sorted Array_2.c
+++ BinarySearch/Medium/153. Find Minimum in Rotated Sorted Array_2.c
@@ -1,6 +1,12 @@
 
 
 
-int findMin(int* nums, int numsSize){
+int findMin(int* nums, int numsSize) {
+	int low = 0;
+	int high = numsSize - 1;
 
+	while(low <= high) {
+		int mid = low + (high - low) / 2;
+		
+	}
 }
