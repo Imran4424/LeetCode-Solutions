a6a786bb90ceb7a45806676a83251a92f26e8311
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-09-09 19:39:34 +0600

    binary search

--- BinarySearch/Medium/153. Find Minimum in Rotated Sorted Array_2.c
+++ BinarySearch/Medium/153. Find Minimum in Rotated Sorted Array_2.c
@@ -1,4 +1,15 @@
+/*
+	4 5 6 7 2 3
 
+	infection point 7 2
+	left of infection point 7
+	right of infection point 2
+	
+	first element of array 4
+
+	left of infection point > first element = 7 > 4
+	right of infection point < first element = 2 < 4
+*/
 
 
 int findMin(int* nums, int numsSize) {
