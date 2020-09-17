567a95b5165de5606e01a99e65dda59f43666e79
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-09-17 15:48:30 +0600

    binary search

--- BinarySearch/Medium/162. Find Peak Element.c
+++ BinarySearch/Medium/162. Find Peak Element.c
@@ -2,5 +2,15 @@
 
 
 int findPeakElement(int* nums, int numsSize){
+	int peakElemnet = nums[0];
+	int peakIndex = 0;
 
+	for (int i = 1; i < numsSize; i++) {
+		if(peakElemnet < nums[i]) {
+			peakElemnet = nums[i];
+			peakIndex = i;
+		}
+	}
+
+	return peakIndex;
 }
