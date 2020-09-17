74b50e545d61f55417917b9f5fa0e1fd95ea4454
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-09-18 02:36:25 +0600

    binary search

--- BinarySearch/Medium/162. Find Peak Element_4.c
+++ BinarySearch/Medium/162. Find Peak Element_4.c
@@ -2,11 +2,17 @@
 /*
 	simulations
 	1 2 3 1
-	low = 0, high = 3, mid = 0 + (3 - 0)/ 2 = 1
+	low = 0, high = 3, mid = 0 + (3 - 0)/2 = 1
 	low = mid + 1 = 1 + 1 = 2, high = 3, mid = 2 + (3 - 2) / 2 = 2
 	low = 2 + 1 = 3, high = 3
 	while condition false
-	break 
+	break
+
+	1 2
+	low = 0, high = 1, mid = 0 + (1 - 0)/2 = 0
+	low = 0 + 1 = 1, high = 1
+	while condition false
+	break
 
 */
 
