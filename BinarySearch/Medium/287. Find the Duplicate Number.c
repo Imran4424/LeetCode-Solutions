74bc7b08f82d779d2fd73d5b95dfd164a8465cf3
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-09-30 16:02:57 +0600

    binary search

--- BinarySearch/Medium/287. Find the Duplicate Number.c
+++ BinarySearch/Medium/287. Find the Duplicate Number.c
@@ -2,5 +2,11 @@
 
 
 int findDuplicate(int* nums, int numsSize) {
-    
+	int vistied[30001] = {0};
+
+	for(int i = 0; i < numsSize; i++) {
+		if(vistied[nums[i]]) {
+			return nums[i];
+		}
+	}    
 }
