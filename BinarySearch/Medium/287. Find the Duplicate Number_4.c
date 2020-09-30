86443d1fd31b14229a81c679391fe2302a65a8ea
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-09-30 21:18:48 +0600

    binary search

--- BinarySearch/Medium/287. Find the Duplicate Number_4.c
+++ BinarySearch/Medium/287. Find the Duplicate Number_4.c
@@ -5,5 +5,18 @@ void mergeSort(int *arr, int size) {
 		return;
 	}
 
-	
+	int mid = size / 2;
+	int left[mid];
+	int right[size - mid];
+
+	for (int i = 0; i < mid; i++) {
+		left[i] = arr[i];
+	}
+
+	for(int i = mid; i < size; i++) {
+		right[i - mid] = arr[i]; 
+	}
+
+	mergeSort(left, mid);
+	mergeSort(right, size - mid);
 }
