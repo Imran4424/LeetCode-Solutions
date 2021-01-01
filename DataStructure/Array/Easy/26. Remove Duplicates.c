c4e1c8ac8f4989396fda86e5c3fde460d7f51f72
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-01-02 02:03:04 +0600

    array

--- DataStructure/Array/Easy/26. Remove Duplicates.c
+++ DataStructure/Array/Easy/26. Remove Duplicates.c
@@ -1,5 +1,19 @@
 
 
 int removeDuplicates(int* nums, int numsSize) {
-    
+	int uniqueNums[numsSize];
+	int index = 0;
+	uniqueNums[index++] = nums[0];
+
+	for (int i = 1; i < numsSize; i++) {
+		if(uniqueNums[index] != nums[i]) {
+			uniqueNums[index++] = nums[i];
+		}
+	}
+
+	for (int i = 0; i < index; i++) {
+		nums[i] = uniqueNums[i];
+	}
+
+	return index;
 }
