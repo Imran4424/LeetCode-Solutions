69a5353970b1082ad42f4ce82a06f52419dc95d1
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-01-02 11:24:06 +0600

    array

--- DataStructure/Array/Easy/27. Remove Element_2.c
+++ DataStructure/Array/Easy/27. Remove Element_2.c
@@ -6,18 +6,13 @@ int removeElement(int* nums, int numsSize, int val){
 	}
 
 	int index = 0;
-	int remainNums[numsSize];
-
+	
 	for (int i = 0; i < numsSize; i++) {
 		if(val == nums[i]) {
 			continue;
 		}
 
-		remainNums[index++] = nums[i];
-	}
-
-	for (int i = 0; i < index; i++) {
-		nums[i] = remainNums[i];
+		nums[index++] = nums[i];
 	}
 
 	return index;
