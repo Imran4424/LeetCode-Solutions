d8a62252092fe87a0d95b2a15d87b7d94c59a85a
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-01-09 03:22:31 +0600

    array

--- DataStructure/Array/Easy/88. Merge Sorted Array.c
+++ DataStructure/Array/Easy/88. Merge Sorted Array.c
@@ -1,5 +1,28 @@
 
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+	int i = 0, j = 0, index = 0;
 
+	int tempArr[m];
+	for (int x = 0; x < m; x++) {
+		tempArr[x] = nums1[x];
+	}
+
+	while (i < m && j < n) {
+		if (tempArr[i] < nums2[j]) {
+			nums1[index] = tempArr[i++];
+		} else {
+			nums1[index] = nums2[j++];
+		}
+
+		index++;
+	}
+
+	while (i < m) {
+		nums1[index++] = tempArr[i++];
+	}
+
+	while(j < n) {
+		nums1[index++] = nums2[j++];
+	}
 }
