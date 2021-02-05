5ec582ad1f8a740410675bc526bcc785dab5b331
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-02-06 02:29:30 +0600

    array

--- DataStructure/Array/Easy/189. Rotate Array.c
+++ DataStructure/Array/Easy/189. Rotate Array.c
@@ -2,7 +2,7 @@
 void rotateRightToLeft(int* nums, int numsSize) {
 	int store = nums[numsSize - 1];
 
-	for (int i = 1; i < numsSize; i++) {
+	for (int i = numsSize - 1; i > 0; i--) {
 		nums[i] = nums[i - 1];
 	}
 
