ba4e68a2247a92a68559bc26893b1c92557026a8
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-03-21 11:09:05 +0600

    heap

---
+++ DataStructure/Heap/Hard/23. Merge k Sorted Lists_3.c
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+	for (int i = 0; i < listsSize; i++) {
+		
+	}
+}
