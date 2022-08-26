6608a206ada33e703da53ecaa10526ad9568691e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2022-08-27 00:00:42 +0600

    math

---
+++ Math/Medium/2221.FindTriangularSumArray.cpp
@@ -0,0 +1,14 @@
+
+
+class Solution {
+public:
+	int triangularSum(vector<int>& nums) {
+		for (int i = 0; i < nums.size(); ++i) {
+			for (int j = 0; j < nums.size() - i - 1; ++j) {
+				nums[j] = (nums[j] + nums[j + 1]) % 10; 
+			}
+		}
+
+		return nums[0];
+	}
+};
