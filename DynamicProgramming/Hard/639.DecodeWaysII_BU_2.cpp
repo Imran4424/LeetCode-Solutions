c55c8839a53a9cafa3ddecf3e05910e4e668aed3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2022-09-25 03:09:27 +0600

    dp

--- DynamicProgramming/Hard/639.DecodeWaysII_BU_2.cpp
+++ DynamicProgramming/Hard/639.DecodeWaysII_BU_2.cpp
@@ -8,29 +8,26 @@ public:
 			return 0;
 		}
 
-		const int codeSize = code.size();
-		int dp[codeSize + 1];
-		for (int i = 0; i <= codeSize; ++i) {
-			dp[i] = 0;
-		}
-
 		// empty set
-		dp[0] = 1;
+		int secondPrev = 1;
 
 		// first element
+		int prev = 1;
 		if ('*' == code[0]) {
-			dp[1] = 9;
+			prev = 9;
 		} else {
-			dp[1] = 1;
+			prev = 1;
 		}
 
 		// start from second Index
 		for (int i = 1; code[i]; ++i) {
+			int current = 0;
+
 			// one digit
 			if('*' == code[i]) {
-				dp[i + 1] = (dp[i + 1] + (9 * (dp[i] % modVal)) % modVal) % modVal;
+				current = (current + (9 * (prev % modVal)) % modVal) % modVal;
 			} else if ('0' != code[i]) {
-				dp[i + 1] = (dp[i + 1] + dp[i]) % modVal;
+				current = (current + prev) % modVal;
 			}
 
 			// two digit
@@ -39,27 +36,30 @@ public:
 
 			if ('*' == code[i - 1]) {
 				if ('*' == code[i]) {
-					dp[i + 1] = (dp[i + 1] + (15 * (dp[i - 1] % modVal)) % modVal) % modVal;
+					current = (current + (15 * (secondPrev % modVal)) % modVal) % modVal;
 				} else if (code[i] <= '6') {
-					dp[i + 1] = (dp[i + 1] + (2 * (dp[i - 1] % modVal)) % modVal) % modVal;
+					current = (current + (2 * (secondPrev % modVal)) % modVal) % modVal;
 				} else {
-					dp[i + 1] = (dp[i + 1] + dp[i - 1]) % modVal;
+					current = (current + secondPrev) % modVal;
 				}
 			} else if ('1' == code[i - 1]) {
 				if ('*' == code[i]) {
-					dp[i + 1] = (dp[i + 1] + (9 * (dp[i - 1] % modVal)) % modVal) % modVal;
+					current = (current + (9 * (secondPrev % modVal)) % modVal) % modVal;
 				} else {
-					dp[i + 1] = (dp[i + 1] + dp[i - 1]) % modVal;
+					current = (current + secondPrev) % modVal;
 				}
 			} else if ('2' == code[i - 1]) {
 				if ('*' == code[i]) {
-					dp[i + 1] = (dp[i + 1] + (6 * (dp[i - 1] % modVal)) % modVal) % modVal;
+					current = (current + (6 * (secondPrev % modVal)) % modVal) % modVal;
 				} else if (code[i] <= '6') {
-					dp[i + 1] = (dp[i + 1] + dp[i - 1]) % modVal;
+					current = (current + secondPrev) % modVal;
 				}
 			}
+
+			secondPrev = prev;
+			prev = current;
 		}
 
-		return dp[codeSize];
+		return prev;
 	}
 };
